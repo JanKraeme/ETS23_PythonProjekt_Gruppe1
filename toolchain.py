8bcc631459b9038982c9c19def99c06d7fb6c486
JanKraeme <jan@d-kraemer.de>
2024-08-09 18:20:17 +0200

    Update toolchain.py

--- toolchain.py
+++ toolchain.py
@@ -1,26 +1,11 @@
-import pyodbc
-# Verbindungsdaten
-server = 'sc-db-server.database.windows.net'
-database = 'supplychain' # Setze den Namen deiner Datenbank hier ein
-username = 'rse'
-password = 'Pa$$w0rd'
-# Verbindungsstring
-conn_str = (
-f'DRIVER={{ODBC Driver 18 for SQL Server}};'
-f'SERVER={server};'
-f'DATABASE={database};'
-f'UID={username};'
-f'PWD={password}'
-)
-# Verbindung herstellen
-conn = pyodbc.connect(conn_str)
-# Cursor erstellen
-cursor = conn.cursor()
-# SQL-Statement ausführen
-cursor.execute('SELECT * FROM coolchain1')
-# Ergebnisse ausgeben
-for row in cursor:
-    print(row)
-# Verbindung schließen
-cursor.close()
-conn.close()
+import tkinter as tk
+from tkinter import filedialog
+
+def choose_file():
+    fenster = tk.Tk()
+    fenster.withdraw()
+
+filedialog = filedialog.askopenfilename()
+
+
+choose_file()
