02e594314391bdf4cf764102a2f817edee8fce34
Kiliantenfelde <142886335+Kiliantenfelde@users.noreply.github.com>
2025-02-19 18:51:08 +0100

    Update wetterabfrage.py

--- Coolchain ETS23/Code/Coolchain 2 ETS23/wetterabfrage.py
+++ Coolchain ETS23/Code/Coolchain 2 ETS23/wetterabfrage.py
@@ -1,19 +1,25 @@
-# Import Meteostat library and dependencies
-from datetime import datetime
-import matplotlib.pyplot as plt
-from meteostat import Point, Daily
-
-# Set time period
-start = datetime(2018, 1, 1)
-end = datetime(2018, 12, 31)
-
-# Create Point for Vancouver, BC
-vancouver = Point(49.2497, -123.1193, 70)
-
-# Get daily data for 2018
-data = Daily(vancouver, start, end)
-data = data.fetch()
-
-# Plot line chart including average, minimum and maximum temperature
-data.plot(y=['tavg', 'tmin', 'tmax'])
-plt.show()
+import requests 
+import json 
+from datetime import datetime 
+ 
+# Beispiel-Nutzung 
+api_key = "NQB3ZXLBGE88NUQHYZFN3QALJ"
+location = "26127,DE" 
+datetime_str = "10.07.2023 13:00"  # Zeit auf die nächste volle Stunde gerundet 
+ 
+# Konvertiere das Datum und die Uhrzeit in das erforderliche Format 
+datetime_obj = datetime.strptime(datetime_str, '%d.%m.%Y %H:%M') 
+timestamp = datetime_obj.strftime('%Y-%m-%dT%H:%M:%S') 
+     
+# Visual Crossing Weather API-Endpunkt 
+url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{timestamp}'.format(location=location, timestamp=timestamp) 
+response = requests.get(url, params={'unitGroup': 'metric','key': 
+api_key,'include': 'hours'}) 
+data = response.json() 
+ 
+# Ausgabe der Temperatur  
+print("\nTemperatur: ", data["days"][0]["temp"],"\n") 
+ 
+# Ausgabe des gesamten JSON-Objekts  
+#json_str = json.dumps(data, indent=4) 
+#print(json_str) 
