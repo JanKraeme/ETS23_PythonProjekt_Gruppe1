45d43195bb3b1f2512f8017bff814b625746faec
Kiliantenfelde <142886335+Kiliantenfelde@users.noreply.github.com>
2025-03-04 13:07:41 +0100

    moin

--- Coolchain ETS23/Code/Coolchain 2 ETS23/Kilian/wettertest_python_weather.py
+++ Coolchain ETS23/Code/Coolchain 2 ETS23/Kilian/wettertest_python_weather.py
@@ -7,6 +7,7 @@ import os
 async def getweather() -> None:
   # declare the client. the measuring unit used defaults to the metric system (celcius, km/h, etc.)
   async with python_weather.Client(unit=python_weather.METRIC) as client:
+      
     # fetch a weather forecast from a city
     weather = await client.get("Oldenburg")
     
