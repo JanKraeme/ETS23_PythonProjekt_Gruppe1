29207d0fa62900c64d6da905122ab0888f71cbb2
chrisru09 <142886642+chrisru09@users.noreply.github.com>
2025-03-04 13:07:49 +0100

    1.1

--- Coolchain ETS23/Code/Jan & Christian/testChristian.py
+++ Coolchain ETS23/Code/Jan & Christian/testChristian.py
@@ -1,64 +1,25 @@
-"""
-# Bibliotheken
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
-# Initialisierung
-key = b'mysecretpassword' # 16 Byte Passwort
-iv = b'passwort-salzen!' # 16 Byte Initialization Vektor
-cipher = AES.new(key, AES.MODE_CBC, iv) # Verschlüsselung initialisieren
-# Entschlüsselung
-ciphertext = b'\xe0\xdc*\x84l\x87;p\xd22\xd9\x94\xabH6\xcd\xf0&\xeduO\x19\x17$+K*wke\x81\xdf'
-plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size) # Text entschlüsseln
-# Ausgabe
-print ('--------------------------------------------------------------------------')
-print ("Entschlüsselter Text als Bytewert: ", plaintext)
-print ("Entschlüsselter Text als String: ", plaintext.decode())
-print ('--------------------------------------------------------------------------')
-"""
-"""
-import pyodbc
-# Verbindungsdaten
-server = 'sc-db-server.database.windows.net'
-database = 'supplychain'
-username = 'rse'
-password = 'Pa$$w0rd'
-
-# Verbindungsstring
-conn_str = (
-    f'DRIVER={{ODBC Driver 18 for SQL Server}};'
-    f'SERVER={server};'
-    f'DATABASE={database};'
-    f'UID={username};'
-    f'PWD={password}'
-)
-
-# Verbindung herstellen
-conn = pyodbc.connect(conn_str)
-
-
-# Cursor erstellen
-cursor = conn.cursor()
-print("Verbindung zur Datenbank hergestellt.")
-# SQL-Statement ausführen
-cursor.execute('SELECT companyID, company, strasse, ort, plz FROM company_crypt')
-# Ergebnisse ausgeben
-for row in cursor:
-    print(row)
-# Verbindung schließen
-cursor.close()
-conn.close()
-"""
-
 import pyodbc
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import unpad
+
+
 # Initialisierung
 key = b'mysecretpassword' # 16 Byte Passwort
 iv = b'passwort-salzen!' # 16 Byte Initialization Vektor
-cipher = AES.new(key, AES.MODE_CBC, iv) # Verschlüsselung initialisieren
+cipher = AES.new(key, AES.MODE_CBC, iv)
+
 # Entschlüsselungsfunktion
+#def decrypt_value(encrypted_data):
+#    return unpad(cipher.decrypt(encrypted_data), AES.block_size).decode()
+
 def decrypt_value(encrypted_data):
-    return unpad(cipher.decrypt(encrypted_data), AES.block_size).decode()
+    try:
+        unpadded_data = unpad(cipher.decrypt(encrypted_data), AES.block_size)
+        return unpadded_data.decode("utf-8", errors="ignore")  # Fehlerbehandlung bei ungültigen Zeichen
+    except ValueError as e:
+        print(f"Fehler bei der Entschlüsselung: {e}")
+        return None
+
 # Verbindungsdaten
 server = 'sc-db-server.database.windows.net'
 database = 'supplychain'
@@ -95,38 +56,48 @@ def load_Company():
         print("")
         print("")
         print(f"ID: {companyID}, Company: {decrypted_company}, Strasse: {decrypted_strasse}, Ort: {decrypted_ort}, PLZ: {decrypted_plz}")
-
         print("")
         print("")
         print("")
         print("")
     #Verbindung schließen
-    cursor.close()
-    conn.close()
-    
+ #   cursor.close()
+
 def lade_Transportdaten():
-    #test
-    # Verbindung herstellen
-    conn = pyodbc.connect(conn_str)
-    cursor = conn.cursor()
-    
+
     select_query = 'SELECT transportstationID, transportstation, category, plz FROM transportstation_crypt'
     cursor.execute(select_query)
-    for row in cursor.fetchall():
-        transportstationID, encrypted_transportstation, encrypted_category, encrypted_plz = row
-        deprypted_transportstation = decrypt_value(encrypted_transportstation)
-        deprypted_category = decrypt_value(encrypted_category)
-        deprypted_plz = decrypt_value(encrypted_plz)
+    results = cursor.fetchall()
+    for row in results:
+        deprypted_transportstation = decrypt_value(row.transportstation)
+        deprypted_category = decrypt_value(row.category)
+        deprypted_plz = decrypt_value(row.plz)
         print("")
-        print("")
-        print("")
-        print(f"ID: {transportstationID}, Transportstation: {deprypted_transportstation}, Kategorie: {deprypted_category}, PLZ: {deprypted_plz}")
-        print("")
-        print("")
-        print("")
+        print(f"ID: {row.transportstationID}, Transportstation: {deprypted_transportstation}, Kategorie: {deprypted_category}, PLZ: {deprypted_plz}")
         print("")
     cursor.close()
-    conn.close()        
+    conn.close()   
 
 load_Company()
 lade_Transportdaten()
+
+
+
+
+# Bibliotheken
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+# Initialisierung
+key = b'mysecretpassword' # 16 Byte Passwort
+iv = b'passwort-salzen!' # 16 Byte Initialization Vektor
+cipher = AES.new(key, AES.MODE_CBC, iv) # Verschlüsselung initialisieren
+# Entschlüsselung
+#ciphertext = b'\xe0\xdc*\x84l\x87;p\xd22\xd9\x94\xabH6\xcd\xf0&\xeduO\x19\x17$+K*wke\x81\xdf'
+ciphertext = b'N\xc8\x17\x95\xdd\xaeY\xa4P\x8e\xd8\x10\xd7P\t#h\xc7z\xd6\x16\xf9\xc6*\x0e8\xb1\x89r\xd1U@'
+plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size) # Text entschlüsseln
+# Ausgabe
+print ('--------------------------------------------------------------------------')
+print ("Entschlüsselter Text als Bytewert: ", plaintext)
+print ("Entschlüsselter Text als String: ", plaintext.decode())
+print ('--------------------------------------------------------------------------')
+
